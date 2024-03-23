c797a55fca6aeca360527bf59cdaf91f9b84730e
BuildTools <unconfigured@null.spigotmc.org>
2024-03-23 01:24:05 -0500

    spawn random balls with cool colors

--- scripts/Manager.gd
+++ scripts/Manager.gd
@@ -1,6 +1,9 @@
 extends Node
 
-@export var ball_scene: PackedScene
+@export var blue_ball: PackedScene
+@export var yellow_ball: PackedScene
+@export var green_ball: PackedScene
+@export var red_ball: PackedScene
 @export var peg_scene: PackedScene
 @export var spawn_count: int = 1
 
@@ -8,9 +11,11 @@ var active_peg: Node
 var cooldown: SceneTreeTimer 
 var cooldown_length: float = .01
 var balls: Array = []
+var ball_scenes: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ball_scenes = [blue_ball, yellow_ball, red_ball, green_ball]
 	cooldown = get_tree().create_timer(0)
 	$SpawnCount.text = str(spawn_count)
 	$Count.text = str(len(balls))
@@ -20,24 +25,25 @@ func _process(delta):
 	if active_peg != null:
 		active_peg.position = get_viewport().get_mouse_position()
 
-func spawn_ball(event: InputEvent):
-	if cooldown.time_left == 0:
-		cooldown = get_tree().create_timer(cooldown_length)
-		for n in spawn_count:
-			await get_tree().create_timer(.01).timeout
-			var ball = ball_scene.instantiate()
-			balls.append(ball)
-			ball.position = event.position
-			var body = ball.get_node("RigidBody2D") as RigidBody2D
-			var max_x = 100
-			var min_x = -100
-			var max_y = 100
-			var min_y = -100
-			body.apply_impulse(Vector2(randf_range(min_x, max_x), randf_range(min_y, max_y)), event.position)
-			if event is InputEventMouseMotion:
-				body.apply_impulse(event.velocity, Vector2(event.relative.x, -event.relative.y))
-			add_child(ball)
-			$Count.text = str(len(balls))
+func spawn_ball(event: InputEvent, scn: PackedScene):
+	if cooldown.time_left != 0:
+		pass
+	cooldown = get_tree().create_timer(cooldown_length)
+	await get_tree().create_timer(.01).timeout
+	
+	var ball = scn.instantiate()
+	balls.append(ball)
+	ball.position = event.position
+	var body = ball.get_node("RigidBody2D") as RigidBody2D
+	var max_x = 100
+	var min_x = -100
+	var max_y = 100
+	var min_y = -100
+	body.apply_impulse(Vector2(randf_range(min_x, max_x), randf_range(min_y, max_y)), event.position)
+	if event is InputEventMouseMotion:
+		body.apply_impulse(event.velocity, Vector2(event.relative.x, -event.relative.y))
+	add_child(ball)
+	$Count.text = str(len(balls))
 
 func _input(event):	
 	if Input.is_action_just_pressed("increase_spawn"):
@@ -54,7 +60,9 @@ func _input(event):
 			
 	if event is InputEventMouseButton:
 		if Input.is_action_just_pressed("spawn_ball"):
-			spawn_ball(event)
+			for n in spawn_count:
+				var random_scn = ball_scenes[randi() % ball_scenes.size()]
+				spawn_ball(event, random_scn)
 					
 		if Input.is_action_just_pressed("spawn_peg"):
 			if active_peg == null:
