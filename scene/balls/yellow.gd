e3da1f0e31a7fcf41fa726dc9b8ed4e5f8283c0e
BuildTools <unconfigured@null.spigotmc.org>
2024-03-26 00:00:41 -0500

    more ball interactions

--- scene/balls/yellow.gd
+++ scene/balls/yellow.gd
@@ -1,9 +1,16 @@
 extends Node2D
-class_name YellowBall
+class_name Yellow
 
 @export var collision_sound: AudioStream
 @export var collision_pitch: float = 10
 @export var max_sounds: int = 3
+@export var max_health: int = 5
+@export var health: int = max_health
+@export var max_scale: Vector2 = Vector2(2, 2)  # Set the maximum scale
+
+@onready var damage: GPUParticles2D = $Yellow/damage
+@onready var sprite_2d: Sprite2D = $Yellow/Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $Yellow/CollisionShape2D
 
 static var sounds_playing: int = 0
 
@@ -14,9 +21,55 @@ func _ready():
 	body.max_contacts_reported = 5
 
 
-func _on_rigid_body_2d_body_entered(body):
-	if sounds_playing < max_sounds:
-		sounds_playing += 1
-		var player: AudioStreamPlayer = SoundManager.play_sound_with_pitch(collision_sound, collision_pitch)
-		await player.finished
-		sounds_playing -= 1
+func take_damage(amount: int):
+	if health > 0:
+		health -= amount
+		damage.emitting = true
+
+
+func heal(amount: int):
+	if health < max_health:
+		health += amount
+
+
+func grow(amount: float):
+	if health == max_health:
+		if sprite_2d.scale.x > max_scale.x:
+			# TODO queue_free particles and += score
+			pass
+		else:
+			sprite_2d.scale += Vector2(0.25, 0.25)
+			collision_shape_2d.scale += Vector2(0.25, 0.25)
+
+
+func touched_by_red():
+	pass
+
+
+func touched_by_green():
+	pass
+
+
+func touched_by_blue():
+	pass
+
+
+func touched_by_yellow():
+	pass
+
+
+func _on_rigid_body_2d_body_entered(body) -> void:
+	return # TODO implement
+#    if body == null:
+#        return
+#    match body.get_name():
+#        "Yellow": touched_by_yellow()
+#        "Red": touched_by_red()
+#        "Green": touched_by_green()
+#        "Blue": touched_by_blue()
+#    if health <= 0:
+#        queue_free()
+#    if sounds_playing < max_sounds:
+#        sounds_playing += 1
+#        var player: AudioStreamPlayer = SoundManager.play_sound_with_pitch(collision_sound, collision_pitch)
+#        sounds_playing -= 1
